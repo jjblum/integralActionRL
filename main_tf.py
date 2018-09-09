d786f9f456324167ab3a2407fc2879b4bd0c6921
jjblum <jason.j.blum@gmail.com>
2018-09-09 22:55:23 +0200

    Added plot of dynamics result at a given goal state.

--- main_tf.py
+++ main_tf.py
@@ -6,13 +6,12 @@ import matplotlib.pyplot as plt
 # from multiprocessing import Process
 # from time import sleep
 
-
 import SimpleOscillator
 
 
 VERBOSE_PRINTOUT_FOR_OSCILLATORS = False
 
-ODE_INSTANCES_BEFORE_LEARNING_STARTS = 50
+ODE_INSTANCES_BEFORE_LEARNING_STARTS = 100
 
 STATE_DIMENSIONS = 2  # position, velocity
 ACTION_DIMENSIONS = 1  # force
@@ -23,7 +22,8 @@ DYN_OUTPUT_DIMENSIONS = STATE_DIMENSIONS
 DYN_HIDDEN_LAYERS_SIZES = [100]
 DYN_DROPOUT_RATE = 0.25
 ACTIVATION = "relu"
-EPOCHS = 30
+EPOCHS = 10
+NORMALIZE_EXPERIENCES = False
 
 ODE_TIME_STEP = 0.1  # seconds
 ODE_DEADLINE = 150  # seconds before ODE is prematurely terminated
@@ -31,9 +31,7 @@ ODE_DEADLINE = 150  # seconds before ODE is prematurely terminated
 OSCILLATOR_M = 1
 OSCILLATOR_K = 0.1
 OSCILLATOR_C = 0.001
-OSCILLATOR_G = 0
-
-
+OSCILLATOR_G = 20
 
 
 def generateReward(oscillator, t):
@@ -62,9 +60,11 @@ def generateReward(oscillator, t):
     return reward, is_terminal
 
 
-def singleODEInstance():
+def singleODEInstance(goal=None):
+    if goal is None:
+        goal = 100*np.random.rand()
     # create the oscillator that will generate experiences
-    osci = SimpleOscillator.SimpleOscillator(m=OSCILLATOR_M, k=OSCILLATOR_K, c=OSCILLATOR_C, goal=50, g=OSCILLATOR_G, max_force=100, control_hz=5, policy="random_pid")
+    osci = SimpleOscillator.SimpleOscillator(m=OSCILLATOR_M, k=OSCILLATOR_K, c=OSCILLATOR_C, goal=goal, g=OSCILLATOR_G, max_force=100, control_hz=5, policy="random_pid")
 
     # create the dynamic size array for experiences
     experiences = list()
@@ -103,7 +103,7 @@ def singleODEInstance():
     return experiences
 
 
-def singleNNInstance(dyn_model):
+def singleNNInstance(dyn_model, dyn_inputs_mean=None, dyn_inputs_stddev=None, dyn_outputs_mean=None, dyn_outputs_stddev=None):
     osci = SimpleOscillator.SimpleOscillator(m=OSCILLATOR_M, k=OSCILLATOR_K, c=OSCILLATOR_C, goal=25, g=OSCILLATOR_G, max_force=100, control_hz=5, policy="pid")
     state_time_history = list()
     action_time_history = list()
@@ -128,9 +128,18 @@ def singleNNInstance(dyn_model):
 
         dyn_input = np.reshape(np.array([old_state[0], old_state[1], action]), (1, DYN_INPUT_DIMENSIONS))
 
+        if NORMALIZE_EXPERIENCES:
+            dyn_input -= np.full(dyn_input.shape, dyn_inputs_mean)
+            dyn_input /= np.full(dyn_input.shape, dyn_inputs_stddev)
+
         osci.setPrevious()
 
         dyn_output = dyn_model.predict(dyn_input)
+
+        if NORMALIZE_EXPERIENCES:
+            dyn_output *= np.full(dyn_output.shape, dyn_outputs_stddev)
+            dyn_output += np.full(dyn_output.shape, dyn_outputs_mean)
+
         new_state = dyn_output[0]
         osci.setState(new_state)
 
@@ -144,13 +153,18 @@ def singleNNInstance(dyn_model):
     final_position = state_time_history[-1, 1]
     final_velocity = state_time_history[-1, 2]
     final_action = action_time_history[-1, 1]
-    print("\tFinal position: {:.2f}\n\tFinal velocity: {:.2f}\n\tFinal action: {:.2f}".format(final_position, final_velocity, final_action))
+    m, k, c, g, f = osci.getPhysics()
+    kinetic_energy = 0.5*m*np.power(final_velocity, 2)
+    potential_energy = 0.5*k*np.power(osci.getGoal(), 2) + OSCILLATOR_M*np.abs(g)
+    total_energy = kinetic_energy + potential_energy
+    terminal_total_energy = 0.5*k*np.power(osci.getGoal(), 2) + m*np.abs(g)  # potential energy only
+    print("\tFinal position: {:.2f}\n\tFinal velocity: {:.2f}\n\tFinal action: {:.2f}, Final energy gap: {:.2f}".format(
+        final_position, final_velocity, final_action, terminal_total_energy - total_energy))
     mean_position = np.mean(state_time_history[:, 1])
     mean_velocity = np.mean(state_time_history[:, 2])
     mean_action = np.mean(action_time_history[:, 1])
     print("\tMean position: {:.2f}\n\tMean velocity: {:.2f}\n\tMean action: {:.2f}".format(mean_position, mean_velocity, mean_action))
 
-    plt.rcParams.update({'font.size': 22})
     fig, ax1 = plt.subplots()
 
     ax1.plot(state_time_history[:, 0], state_time_history[:, 1], 'r')
@@ -179,6 +193,34 @@ def singleNNInstance(dyn_model):
     # TODO:     current velocity is small and action is low, right?
     # TODO:     I just flat out do not understand why the NN is saying the velocity oscillates like you expect, but oscillates around a nonzero value you wouldn't
 
+def visualizeActionAroundGoal(dyn_model, goal=25, n=100):
+    # TODO:     NEW IDEA: what if I, after training, apply the NN with (goal, 0, linspace(-100, 100, 100))
+    # TODO:     i.e. apply the NN with the goal state and a range of actions to see what it predicts for
+    # TODO:     the range of possible actions.
+    # TODO:     Plot the result, x axis is range of actions, y axis 1 is next position, y axis 2 is next velocity
+    # TODO:     Maybe that way I can see where the prediction is accurate and where it is inaccurate
+    actions = np.linspace(-100, 100, n)
+    actions = np.atleast_2d(actions)
+    actions = actions.T
+    dyn_input = np.hstack((np.full((n, 2), fill_value=[goal, 0]), actions))
+    actions = np.squeeze(actions)
+    dyn_output = dyn_model.predict(dyn_input)
+
+    fig, ax1 = plt.subplots()
+    ax1.plot(actions, dyn_output[:, 0], 'r')
+    ax1.set_xlabel("action around goal state [{:.2f}, 0]".format(goal))
+    ax1.set_ylabel("predicted position", color = 'r')
+    ax1.tick_params('y', colors='r')
+    plt.grid(True)
+
+    ax2 = ax1.twinx()
+    ax2.plot(actions, dyn_output[:, 1], 'b')
+    #ax2.set_xlabel("action around goal state [{:.2f}, 0]".format(goal))
+    ax2.set_ylabel("predicted velocity", color = 'b')
+    ax2.tick_params('y', colors='b')
+
+    plt.grid(True)
+    plt.show()
 
 
 def randomExperienceReplay(experiences, size_of_subset=None):
@@ -227,14 +269,31 @@ def main(sess=None):
     # TODO: minibatch learn the experiences subset
     # alternate learning the dynamics model via experience replay and generating more experiences
     dyn_batch_inputs, dyn_batch_outputs = randomDynamicsReplay(experiences, len(experiences))
-    dyn_model.fit(dyn_batch_inputs, dyn_batch_outputs, epochs=EPOCHS, verbose=True, batch_size=32)
+
+    if NORMALIZE_EXPERIENCES:
+        dyn_batch_inputs_mean = np.mean(dyn_batch_inputs, axis=0)
+        dyn_batch_inputs_stddev = np.std(dyn_batch_inputs, axis=0)
+        dyn_batch_inputs -= np.full(dyn_batch_inputs.shape, dyn_batch_inputs_mean)
+        dyn_batch_inputs /= np.full(dyn_batch_inputs.shape, dyn_batch_inputs_stddev)
+
+        dyn_batch_outputs_mean = np.mean(dyn_batch_outputs, axis=0)
+        dyn_batch_outputs_stddev = np.std(dyn_batch_outputs, axis=0)
+        dyn_batch_outputs -= np.full(dyn_batch_outputs.shape, dyn_batch_outputs_mean)
+        dyn_batch_outputs /= np.full(dyn_batch_outputs.shape, dyn_batch_outputs_stddev)
+
+    dyn_model.fit(dyn_batch_inputs, dyn_batch_outputs, epochs=EPOCHS, verbose=True)
 
     # TODO: after X minibatches have been learned...
     # TODO:     1) generate dropout samples of the NN
     # TODO:     2) perform a physics rollout, replacing the ODE integration with the NN dropout samples (timestep = control Hz timestep, or ODE timestep???)
     # TODO:     3) visually compare the ODE integration vs. population of NN dropout sample rollouts - MUST USE FIXED POLICY FOR ALL ROLLOUTS!!!
 
-    singleNNInstance(dyn_model)
+    # visualizeActionAroundGoal(dyn_model)
+
+    if NORMALIZE_EXPERIENCES:
+        singleNNInstance(dyn_model, dyn_batch_inputs_mean, dyn_batch_inputs_stddev, dyn_batch_outputs_mean, dyn_batch_outputs_stddev)
+    else:
+        singleNNInstance(dyn_model)
 
     # TODO: could the nonzero velocity be because position is larger than velocity? Maybe that dominates the signal because I'm usign Relu units.
 
@@ -242,4 +301,5 @@ def main(sess=None):
 
 
 if __name__ == "__main__":
+    plt.rcParams.update({'font.size': 22})
     main()
