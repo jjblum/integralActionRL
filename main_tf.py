8e750ad34e6a03ce725f5f1e4945114b8140b291
jjblum <jason.j.blum@gmail.com>
2018-10-02 01:06:43 +0200

    Resampling of a sequence of experiences to avoid overweighting near equillibrium.

--- main_tf.py
+++ main_tf.py
@@ -19,19 +19,23 @@ EXPERIENCE_DIMENSIONS = STATE_DIMENSIONS*2 + ACTION_DIMENSIONS + 1  # state 0, a
 
 DYN_INPUT_DIMENSIONS = STATE_DIMENSIONS + ACTION_DIMENSIONS
 DYN_OUTPUT_DIMENSIONS = STATE_DIMENSIONS
-DYN_HIDDEN_LAYERS_SIZES = [20, 20]  # [100]
+DYN_HIDDEN_LAYERS_SIZES = [100, 100]
 DYN_HIDDEN_ACTIVATIONS = ["linear", "linear"]
-DYN_DROPOUT_RATE = 0.25
-EPOCHS = 125
+DYN_DROPOUT_RATE = 0.5
+EPOCHS = 60
 NORMALIZE_EXPERIENCES = False
 
 ODE_TIME_STEP = 0.1  # seconds
-ODE_DEADLINE = 150  # seconds before ODE is prematurely terminated
+ODE_DEADLINE = 300  # seconds before ODE is prematurely terminated
 
 OSCILLATOR_M = 1
 OSCILLATOR_K = 0.1
 OSCILLATOR_C = 0.001
-OSCILLATOR_G = 20
+OSCILLATOR_G = 50
+
+TEST_GOAL = 25
+TEST_FINAL_ACTION = OSCILLATOR_K*TEST_GOAL - OSCILLATOR_G
+TEST_FINAL_VELOCITY = 0
 
 
 def generateReward(oscillator, t):
@@ -62,7 +66,7 @@ def generateReward(oscillator, t):
 
 def singleODEInstance(goal=None):
     if goal is None:
-        goal = 20*np.random.rand() + 10
+        goal = TEST_GOAL
     # create the oscillator that will generate experiences
     osci = SimpleOscillator.SimpleOscillator(m=OSCILLATOR_M, k=OSCILLATOR_K, c=OSCILLATOR_C, goal=goal, g=OSCILLATOR_G, max_force=100, control_hz=5, policy="random_pid")
 
@@ -101,11 +105,25 @@ def singleODEInstance(goal=None):
     if not terminal:
         # print("ODE oscillator did not reach terminal state in {} seconds, terminating".format(ODE_DEADLINE))
         None
-    return experiences
+
+    # TODO: resample so redundant transitions are not included
+    experiences_array = np.array(experiences)
+    interesting_experiences = [experiences_array[0]]
+    for i in range(len(experiences)):
+        d = np.linalg.norm(experiences_array[i, :2] - interesting_experiences[-1][:2])
+        if d > 0.1:
+            interesting_experiences.append(experiences_array[i])
+            # print("{} ADD".format(d))
+        else:
+            None
+            # print("{} skip".format(d))
+    interesting_experiences.append(experiences_array[-1])
+
+    return [a.tolist() for a in interesting_experiences]
 
 
 def singleNNInstance(dyn_model, dyn_inputs_mean=None, dyn_inputs_stddev=None, dyn_outputs_mean=None, dyn_outputs_stddev=None):
-    osci = SimpleOscillator.SimpleOscillator(m=OSCILLATOR_M, k=OSCILLATOR_K, c=OSCILLATOR_C, goal=25, g=OSCILLATOR_G, max_force=100, control_hz=5, policy="pid")
+    osci = SimpleOscillator.SimpleOscillator(m=OSCILLATOR_M, k=OSCILLATOR_K, c=OSCILLATOR_C, goal=TEST_GOAL, g=OSCILLATOR_G, max_force=100, control_hz=5, policy="pid")
     state_time_history = list()
     action_time_history = list()
     print("Running a single instance of the dynamics NN oscillator")
@@ -169,6 +187,7 @@ def singleNNInstance(dyn_model, dyn_inputs_mean=None, dyn_inputs_stddev=None, dy
     fig, ax1 = plt.subplots()
 
     ax1.plot(state_time_history[:, 0], state_time_history[:, 1], 'r')
+    ax1.plot([0, state_time_history[-1, 0]], [TEST_GOAL, TEST_GOAL], 'r--')
     ax1.set_xlabel('time (s)')
     ax1.set_ylabel('dyn NN position', color="r")
     ax1.tick_params('y', colors='r')
@@ -177,6 +196,8 @@ def singleNNInstance(dyn_model, dyn_inputs_mean=None, dyn_inputs_stddev=None, dy
 
     ax2 = ax1.twinx()
     ax2.plot(state_time_history[:, 0], state_time_history[:, 2], 'b')
+    ax2.plot([0, state_time_history[-1, 0]], [TEST_FINAL_VELOCITY, TEST_FINAL_VELOCITY], 'b--')
+    #ax2.get_yaxis().set_visible(False)
     ax2.set_xlabel('time (s)')
     ax2.set_ylabel('dyn NN velocity', color="b")
     ax2.tick_params('y', colors='b')
@@ -184,6 +205,8 @@ def singleNNInstance(dyn_model, dyn_inputs_mean=None, dyn_inputs_stddev=None, dy
 
     ax3 = ax1.twinx()
     ax3.plot(action_time_history[:, 0], action_time_history[:, 1], 'g')
+    ax3.plot([0, action_time_history[-1, 0]], [TEST_FINAL_ACTION, TEST_FINAL_ACTION], 'g--')
+    #ax3.get_yaxis().set_visible(False)
     ax3.set_xlabel('time (s)')
     ax3.set_ylabel('\ndyn NN action', color="g")
     ax3.tick_params('y', colors='g')
@@ -198,7 +221,7 @@ def singleNNInstance(dyn_model, dyn_inputs_mean=None, dyn_inputs_stddev=None, dy
     # TODO:     current velocity is small and action is low, right?
     # TODO:     I just flat out do not understand why the NN is saying the velocity oscillates like you expect, but oscillates around a nonzero value you wouldn't
 
-def visualizeActionAroundGoal(dyn_model, goal=25, n=100):
+def visualizeActionAroundGoal(dyn_model, goal=TEST_GOAL, n=100):
     # TODO:     NEW IDEA: what if I, after training, apply the NN with (goal, 0, linspace(-100, 100, 100))
     # TODO:     i.e. apply the NN with the goal state and a range of actions to see what it predicts for
     # TODO:     the range of possible actions.
@@ -214,14 +237,14 @@ def visualizeActionAroundGoal(dyn_model, goal=25, n=100):
     fig, ax1 = plt.subplots()
     ax1.plot(actions, dyn_output[:, 0], 'r')
     ax1.set_xlabel("action around goal state [{:.2f}, 0]".format(goal))
-    ax1.set_ylabel("predicted position", color = 'r')
+    ax1.set_ylabel("predicted position", color='r')
     ax1.tick_params('y', colors='r')
     plt.grid(True)
 
     ax2 = ax1.twinx()
     ax2.plot(actions, dyn_output[:, 1], 'b')
     #ax2.set_xlabel("action around goal state [{:.2f}, 0]".format(goal))
-    ax2.set_ylabel("predicted velocity", color = 'b')
+    ax2.set_ylabel("predicted velocity", color='b')
     ax2.tick_params('y', colors='b')
 
     plt.grid(True)
@@ -251,11 +274,11 @@ def main(sess=None):
 
     experiences = list()
     for _ in range(ODE_INSTANCES_BEFORE_LEARNING_STARTS):
-        experiences.extend(singleODEInstance())
+        experiences.extend(singleODEInstance(-50 + 100*np.random.rand()))
 
     # TODO: do we need to normalize (mean = 0, std.dev. = 1) the experiences?
 
-    # TODO: setup a simple Relu network representing state+action->new state transitions
+    # setup a simple Relu network representing state+action->new state transitions
     dyn_model = keras.Sequential()
 
     n = 1
@@ -268,9 +291,9 @@ def main(sess=None):
         dyn_model.add(keras.layers.Dropout(DYN_DROPOUT_RATE, name="dyn_dropout_" + str(n)))
 
     dyn_model.add(keras.layers.Dense(DYN_OUTPUT_DIMENSIONS, activation="linear", name="dyn_output"))
-    dyn_model.compile(loss="mean_squared_error", optimizer=keras.optimizers.Adam())
+    # dyn_model.compile(loss="mean_squared_error", optimizer=keras.optimizers.Adam())
+    dyn_model.compile(loss="mean_absolute_error", optimizer=keras.optimizers.Adam())  # TODO: try different losses
 
-    # TODO: minibatch learn the experiences subset
     # alternate learning the dynamics model via experience replay and generating more experiences
     dyn_batch_inputs, dyn_batch_outputs = randomDynamicsReplay(experiences, len(experiences))
 
@@ -285,7 +308,7 @@ def main(sess=None):
         dyn_batch_outputs -= np.full(dyn_batch_outputs.shape, dyn_batch_outputs_mean)
         dyn_batch_outputs /= np.full(dyn_batch_outputs.shape, dyn_batch_outputs_stddev)
 
-    dyn_model.fit(dyn_batch_inputs, dyn_batch_outputs, epochs=EPOCHS, verbose=2, validation_split=0.05, batch_size=128, shuffle=True)
+    dyn_model.fit(dyn_batch_inputs, dyn_batch_outputs, epochs=EPOCHS, verbose=2, batch_size=256, shuffle=True)  # validation_split=0.05
 
     # TODO: after X minibatches have been learned...
     # TODO:     1) generate dropout samples of the NN
@@ -307,3 +330,8 @@ def main(sess=None):
 if __name__ == "__main__":
     plt.rcParams.update({'font.size': 22})
     main()
+
+    # TODO:  MAJOR IDEA TO AVOID TOO MANY EXPERIENCES CLUSTERED AROUND THE ZERO (WHERE THE OSCILLATOR SLOWS WAY DOWN)
+    # TODO:  resample! can't resample based on time...
+    # TODO:  but could resample based on state? e.g. only retain an experience if it is sufficiently far from the previous one
+    # TODO:  that way, you won't have a *huge* number of equillibrium transitions
